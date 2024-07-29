48cc4b79460779c723c04e02ed93ff24b70b0fdb
ArkoSammy12 <jamesvillarreal12@gmail.com>
2024-07-29 16:57:34 -0500

    Init commit

---
+++ src/client/kotlin/xd/arkosammy/compsmpdiscordbot/CompSMPDiscordBotClient.kt
@@ -0,0 +1,8 @@
+package xd.arkosammy.compsmpdiscordbot
+
+import net.fabricmc.api.ClientModInitializer
+
+object CompSMPDiscordBotClient : ClientModInitializer {
+	override fun onInitializeClient() {
+	}
+}
---
+++ src/main/kotlin/xd/arkosammy/compsmpdiscordbot/CompSMPDiscordBot.kt
@@ -0,0 +1,14 @@
+package xd.arkosammy.compsmpdiscordbot
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object CompSMPDiscordBot : ModInitializer {
+
+	const val MOD_ID: String = "compsmpdiscordbot"
+    private val LOGGER = LoggerFactory.getLogger(MOD_ID)
+
+	override fun onInitialize() {
+
+	}
+}
